5c8d5ca405a24b05625bca80f69de393abe47efc
Maksym Arabadzhyi <maksym.arabadzhyi@valtech.com>
2023-12-14 18:33:41 +0200

    Added PDP

--- frontend/src/components/Product/Product.tsx
+++ frontend/src/components/Product/Product.tsx
@@ -12,13 +12,13 @@ const Product = ({product}: IProductProps) => {
 
 			<Card.Body>
 				<Link to={`/product/${product._id}`}>
-					<Card.Title as="div">
+					<Card.Title as="div" className="product-title">
 						<strong>{product.name}</strong>
 					</Card.Title>
 				</Link>
 
 				<Card.Text as="div">
-					<Rating value={product.rating} />
+					<Rating value={product.rating} text={`${product.numReviews} reviews`} />
 				</Card.Text>
 
 				<Card.Text as="h3">{product.price}</Card.Text>
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,12 +1,13 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
 
 import App from '../App';
-import HomeScreen from '../screens/HomeScreen';
+import {HomeScreen, ProductScreen} from '../screens';
 
 export const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
 			<Route index={true} path="/" element={<HomeScreen />}></Route>
+			<Route path="/product/:id" element={<ProductScreen />}></Route>
 		</Route>,
 	),
 );
